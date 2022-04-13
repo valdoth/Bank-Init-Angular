dd06c1adb2970ee787254d25436fe1ce994cb39f
valdoth <valdotsiarohasina@gmail.com>
2022-04-13 14:22:57 +0300

    add component breadcrumb header

--- src/app/features/layout/layout.module.ts
+++ src/app/features/layout/layout.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { LayoutRoutingModule } from './layout-routing.module';
+import { BreadcrumbComponent } from './breadcrumb/breadcrumb.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    BreadcrumbComponent
+  ],
   imports: [
     CommonModule,
     LayoutRoutingModule
