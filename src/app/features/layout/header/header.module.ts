27f074cd1786fd88cad2b203f952b9aaac1b81bb
nirinaramamy1 <nirinaramamy1@gmail.com>
2022-04-13 12:46:40 +0300

    ajout du dossier images dans assets

---
+++ src/app/features/layout/header/header.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SidemenuComponent } from '../sidemenu/sidemenu.component';
+
+
+@NgModule({
+  declarations: [
+    SidemenuComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class HeaderModule { }
