86f8c4cc4b81d0a1d42601879044ae8028c868ad
valdoth <valdotsiarohasina@gmail.com>
2022-04-13 15:01:31 +0300

    merge breadcrumb et header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { BreadcrumbComponent } from './features/layout/breadcrumb/breadcrumb.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BreadcrumbComponent
   ],
   imports: [
     BrowserModule,
