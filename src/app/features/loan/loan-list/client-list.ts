72c1c54d2a52a616f2a943f894c837aca44fa5fa
nirinaramamy1 <nirinaramamy1@gmail.com>
2022-05-04 13:10:27 +0300

    Ajout liste

---
+++ src/app/features/loan/loan-list/client-list.ts
@@ -0,0 +1,10 @@
+export interface IClientList {
+    client: String,
+    montant_pret: number,
+    date_du_pret: String,
+    fin_du_contract: String,
+    montant_interne: number,
+    pourcentage: number,
+    mode_de_paiement: String
+}
+
--- src/app/features/loan/loan.module.ts
+++ src/app/features/loan/loan.module.ts
@@ -2,14 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoanCreateComponent } from './loan-create/loan-create.component';
 import { LoanListComponent } from './loan-list/loan-list.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     LoanCreateComponent,
-    LoanListComponent
+    LoanListComponent,
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule
   ],
   exports : [
     LoanCreateComponent,
